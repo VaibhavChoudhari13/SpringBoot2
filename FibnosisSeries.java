e2441987eaab311929d1f212ac5bc363930c48a7
VaibhavChoudhari13 <13vaibhavschoudhari@gmail.com>
2022-03-25 21:24:53 -0700

    first ciommit

---
+++ FibnosisSeries.java
@@ -0,0 +1,34 @@
+package com.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class FibnosisSeries {
+
+	public static void main(String[] args) {
+		
+		
+		  int a = 0 ; 
+		  int b = 1 ; 
+		  int c ;
+		  
+		  
+		  for(int i = 0 ; i<=11 ; i++) 
+		  { 
+			  c = a + b ;
+		  
+			    a = b ; 
+		  		b = c ;
+		  
+		  System.out.println(c); }
+		 
+		
+	 
+		
+ 		
+		
+	}
+	
+	
+}
